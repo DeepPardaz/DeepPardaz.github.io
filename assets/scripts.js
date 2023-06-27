f172676141daea9c32f7cc61a85645a3600cc35a
Hadi Heydari <hadiheydarimw1375@gmail.com>
2023-06-27 11:05:41 +0200

    Update scripts.js

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,7 +1,30 @@
-window.addEventListener('DOMContentLoaded', function() {
-  var elements = document.getElementsByClassName('fade-in');
-  
-  Array.prototype.forEach.call(elements, function(element) {
-    element.classList.add('show');
-  });
+document.addEventListener("DOMContentLoaded", function() {
+  const animationContainer = document.querySelector(".animation-container");
+  const animationElement = document.getElementById("animation");
+  const text = "01010101010101010101010101010101";
+  const word = "Deep Pardaz";
+  const animationDuration = 3500; // Animation duration in milliseconds
+
+  let currentText = "";
+  let currentIndex = 0;
+
+  // Function to update the animation text
+  function updateAnimation() {
+    if (currentIndex >= text.length) {
+      animationElement.textContent = word;
+      return;
+    }
+
+    currentText += text[currentIndex];
+    animationElement.textContent = currentText;
+    currentIndex++;
+
+    // Scroll animation
+    animationContainer.scrollTop = animationContainer.scrollHeight;
+
+    setTimeout(updateAnimation, 100); // Delay between each character
+  }
+
+  // Start the animation
+  setTimeout(updateAnimation, 1000); // Delay before animation starts
 });
